93031fa4583249fc12b68f94daa845cea86c636d
jimd1989 <jamesdalrymple@protonmail.com>
2022-08-20 16:15:58 +1000

    Hello world

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,7 +1,23 @@
 module Main (main) where
 
-import Prelude (IO)
-import Lib
+import Prelude ((.), ($), pure)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.ByteString.Lazy.Char8 (fromChunks)
+import Data.Text (Text)
+import Data.Text.Encoding (encodeUtf8)
+import Network.HTTP.Types (Status, status200)
+import Network.HTTP.Types.Header (hContentType)
+import Network.Wai (Request, Response, pathInfo, requestMethod, responseLBS)
+import Network.Wai.Handler.Warp (run)
 
-main :: IO ()
-main = someFunc
+main ∷ MonadIO m ⇒ m ()
+main = liftIO $ run 1917 $ \req send → send $ route req 
+
+route ∷ Request → Response
+route α = case (pathInfo α, requestMethod α) of
+  (_, "POST") → textResponse status200 "hello POST"
+  (_, _     ) → textResponse status200 "hello GET"
+
+textResponse ∷ Status → Text → Response
+textResponse α ω = responseLBS α [(hContentType, "text/plain")] (convert ω)
+  where convert = fromChunks . pure . encodeUtf8
